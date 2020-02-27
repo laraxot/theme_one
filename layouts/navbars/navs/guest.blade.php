835ba4054ab1b2d99b2745583cd155ba8626e870
Dade <davidecavallini1987@gmail.com>
2020-02-27 16:46:34 +0100

    pezzi obbligatori, messi vuoti

--- layouts/navbars/navs/guest.blade.php
+++ layouts/navbars/navs/guest.blade.php
@@ -1,8 +1,12 @@
 <!-- Navbar -->
+@php
+ $activePage='altro'; //ste cose perche' ho preso un tema pre costruito
+ $backgroundImage ='';
+@endphp
 <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
   <div class="container-fluid">
     <div class="navbar-wrapper">
-    <a class="navbar-brand" href="#pablo">{{ $namePage }}</a>
+    <a class="navbar-brand" href="#pablo">{{ $namePage ?? ''}}</a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-bar navbar-kebab"></span>
