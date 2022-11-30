afe964ecc927ebb4a32f9f3ea18d235c863016cf
marco xot <marco.sottana@gmail.com>
2022-11-30 17:42:43 +0100

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,26 +1,40 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
+const path = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+const dotenvExpand = require('dotenv-expand');
+dotenvExpand(require('dotenv').config({ path: '../../.env', debug: true}));
 
-mix.autoload({
-    jquery: ['$', 'window.jQuery', 'jQuery'],
-    tether: ['window.Tether', 'Tether'],
-    'tether-shepherd': ['Shepherd'],
-    'popper.js/dist/popper.js': ['Popper']
-})
-    .js('resources/js/app.js', 'dist/js')
-    .sass('resources/sass/app.scss', 'dist/css')
-    .options({
-        postCss: [
-            require('precss')()
+//require('laravel-mix-merge-manifest');
+//mix.setPublicPath('../../public').mergeManifest();
+
+var public_path = 'Resources/dist';
+
+
+mix.js('Resources/js/app.js', 'js');
+mix.sass('Resources/scss/app.scss', 'css');
+
+mix.setPublicPath(public_path);
+
+mix.extract();
+
+mix.webpackConfig({
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules')
         ]
-    });
+    },
+
+});
+
+//mix.version();
+mix.sourceMaps();
+
+var $from = './Resources/dist';
+var $to = '../../../public_html/themes/One/dist';
+console.log('from :' + $from);
+console.log('to :' + $to);
+
+//UnhandledPromiseRejectionWarning: Error: ENOENT: no such file or directory, open 'F:\var\www\base_xxx\laravel\Themes\AdminLTE\Resources\views\dist\public_html\themes\AdminLTE\dist\admin-lte.css'
+//Error: ENOENT: no such file or directory, open 'F:\Resources\views\dist\css\admin-lte.css'
+
+mix.copyDirectory($from, $to);
