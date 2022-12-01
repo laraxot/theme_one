be862f515938f574b2fc114fdd30b9f5bf9c8603
marco xot <marco.sottana@gmail.com>
2022-12-01 17:37:02 +0100

    up

--- Resources/js/echo.js
+++ Resources/js/echo.js
@@ -1,21 +1,3 @@
-//import Echo from 'laravel-echo';
-
-//window.Pusher = require('pusher-js');
-
-/*window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_PUSHER_HOST,
-    //wsPath: '/socket',
-    wsPort: process.env.MIX_PUSHER_PORT,
-    wssPort: process.env.MIX_PUSHER_PORT,
-    forceTLS: false,
-    encrypted: true,
-    enableLogging: true,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-});*/
-
 import Echo from "laravel-echo"
 
 window.Pusher = require('pusher-js');
@@ -28,8 +10,11 @@ window.Echo = new Echo({
     wssPort: process.env.MIX_PUSHER_PORT,
     wsPath: process.env.MIX_PUSHER_PATH,
     wssPath: process.env.MIX_PUSHER_PATH,
-    forceTLS: false,
-    disableStats: false,
+    forceTLS: true,
+    encrypted: true,
+    enableLogging: true,
+    disableStats: true,
+    enabledTransports: ['ws', 'wss']
 });
 
 console.log({
