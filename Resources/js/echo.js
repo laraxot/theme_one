d6f2d8cecb7d76457225f16cd571ed9b03378fb4
marco xot <marco.sottana@gmail.com>
2022-12-05 19:13:10 +0100

    up

--- Resources/js/echo.js
+++ Resources/js/echo.js
@@ -5,20 +5,14 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_LARAVEL_WEBSOCKET_ADDRESS,
-    wsPort: process.env.MIX_PUSHER_PORT,
-    wssPort: process.env.MIX_PUSHER_PORT,
-    wsPath: process.env.MIX_PUSHER_PATH,
-    wssPath: process.env.MIX_PUSHER_PATH,
+    wsHost: 'socket.mediamonitor.it',
+    wsPort: 80,
+    wssHost: 'socket.mediamonitor.it',
+    wssPort: 443,
+    //encrypted: true,
     //forceTLS: false,
-    encrypted: true,
-    enableLogging: true,
     disableStats: false,
+    enableLogging: true,
     enabledTransports: ['ws', 'wss'],
     scheme: 'https'
 });
-
-console.log({
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_LARAVEL_WEBSOCKET_ADDRESS
-})
