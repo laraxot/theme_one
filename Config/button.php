0ad9cbd46b5f45332e5cdeb44e9d061f8b57dee1
Malebestia <vair81@gmail.com>
2023-06-10 12:23:52 +0200

    up

---
+++ Config/button.php
@@ -0,0 +1,14 @@
+<?php return array (
+  'v1' => 
+  array (
+    'class' => 'btn btn-primary',
+    'submit' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+    'button' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+  ),
+);
---
+++ Resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <!-- Simple Bootstrap Alert-->
+    <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
+    
+    {{--
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button>
+
+
+    <livewire:broad />broad
+
+    <livewire:card.try-card />
+    --}}
+    
+</x-app-layout>
